7b61e12f8604213a6e173ee2a4bdfedea0feaf35
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-05-24 15:51:10 +0300

    message

--- hodi33/24-5-functions.py
+++ hodi33/24-5-functions.py
@@ -1,8 +1,58 @@
-def my_max(a, b):
-    return (a if a > b else b)
+# def my_max(a, b):
+#     return (a if a > b else b)
+#
+#
+# #      => |function| =>
+#
+# print(my_max(5, 7))
+# # max = my_max(5, 111)
 
 
-#      => |function| =>
+# def greet():
+#     print('hello')
+#
+#
+# def greet_with_name(name, age, salary):
+#     print(age)
+#     print(name)
+#     print(salary)
 
-print(my_max(5, 7))
-# max = my_max(5, 111)
+#
+# greet_with_name('moshe', 9, 99000)
+# greet_with_name(age=16, name='yael', salary=1000)
+#
+# st1 = ['eran', 25, 120000]
+# greet_with_name(st1[0], st1[1], st1[2])
+# name = 'maxim'
+# age = '27'
+# salary = 88999
+# greet_with_name(name,age,salary)
+#
+# def foo(name):
+#     print(name)
+
+#
+# x = 5
+#
+#
+# def foo():
+#     x=12
+#     print(x)
+#
+#
+# foo()
+
+# Write a Python function to reverse a string.
+# Sample String : "1234abcd"
+# Expected Output : "dcba4321"
+
+# Write a Python function to check whether a
+# number falls within a given range.
+# the function must receive 3 args
+# value  max min        3   5 9    5   -9   11
+#
+# max    value     min
+
+
+# Write a Python program to detect
+# the number of local variables declared in a function.
