3ac6d80144eb340edcde0a7e88405e053981e55d
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-06-05 15:45:07 +0300

    wolt

--- hodi33/5-6/req.py
+++ hodi33/5-6/req.py
@@ -6,11 +6,12 @@
 
 import requests
 
-word = input('please enter a word')
-print(word)
+with open('milon.txt','a+') as file:
+    word = input('please enter a word')
+    print(word)
 
-url = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + word
-res = requests.get(url)
-word_data = res.json()
-print(word_data[0]['word'])
-print(word_data[0]['meanings'][0]['definitions'][0]['definition'])
+    url = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + word
+    res = requests.get(url)
+    word_data = res.json()
+    file.write(word_data[0]['word'] +' : ')
+    file.write(word_data[0]['meanings'][0]['definitions'][0]['definition'] +'\n\n')
