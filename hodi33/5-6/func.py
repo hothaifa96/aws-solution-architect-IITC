207361f335d89ee03bfc43384ba59a5c2518c184
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-06-05 14:03:54 +0300

    wolt

---
+++ hodi33/5-6/func.py
@@ -0,0 +1,17 @@
+# def my_max(item):
+#     return max(item)
+#
+#
+
+
+dog={
+    'name':'gibour',
+    'age': 74,
+    'type':'poodle',
+    'color':'light blue'
+}
+
+print(dog)
+print(dog.values())
+print(dog.keys())
+print(dog.items())
