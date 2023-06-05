79346fe514acb38f6cf8729461b6cc97a6c850e2
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-06-05 16:15:03 +0300

    wolt

--- hodi33/5-6/req.py
+++ hodi33/5-6/req.py
@@ -4,14 +4,25 @@
 # search for the definitions and word keys
 # insert them text file using the commands you learned
 
-import requests
+# import requests
+#
+# with open('milon.txt','a+') as file:
+#     word = input('please enter a word')
+#     print(word)
+#
+#     url = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + word
+#     res = requests.get(url)
+#     word_data = res.json()
+#     file.write(word_data[0]['word'] +' : ')
+#     file.write(word_data[0]['meanings'][0]['definitions'][0]['definition'] +'\n\n')
 
-with open('milon.txt','a+') as file:
-    word = input('please enter a word')
-    print(word)
 
-    url = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + word
-    res = requests.get(url)
-    word_data = res.json()
-    file.write(word_data[0]['word'] +' : ')
-    file.write(word_data[0]['meanings'][0]['definitions'][0]['definition'] +'\n\n')
+l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
+
+
+a = lambda b: b**2
+
+c= a(15)
