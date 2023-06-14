778a93306636547bf976512b85e463e070e5ce4e
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-06-14 09:23:17 +0300

    wolt

---
+++ hodi33/wrapup.py
@@ -0,0 +1,102 @@
+# a = 12 // 5
+# a = float  int str boolean
+#
+#
+# x = 'hothaifa'
+# x = bool(x)  -> True
+#
+# bool(0.1) True
+
+## this code print the div of 2 number
+
+# x = int(input('enter a number '))
+# y = int(input('enter a number '))
+#
+# if y != 0:
+#     print(x / y)
+# else:
+#     print('cannot div by zero')
+
+# [0,1,2,3,4]
+# for x in range(5):
+#     print(x)
+#
+# # 0,1,2,3,4,5
+# # 1 2 3 4 5
+# # 0 1 2 3 4     V
+# # 0,1,2,3,4
+
+# while True:
+#     x = input()
+#     if x.upper() != 'Q':
+#         break
+#     print(x)
+#
+
+# while for
+# import json
+#
+# {} () [] {,} fifo lifo
+#
+# {:,:} JSON-str
+# json.dumps()
+
+# PUT PATCH
+# GET POST
+#
+# HTTPS HTTP
+#
+# 200 3001 301 404 4018 599
+
+# -> -
+# -> ->
+# - ->
+# - -
+
+# return
+# def hot(a,b,c=0):
+#     pass
+
+
+# requests -
+# pip3 -
+# how to run python file -
+# python main.py &
+#
+# a+ r+
+#
+# open()
+# file.close()
+#
+# with:
+#
+import datetime
+import time
+
+# x = 3
+# while x > 1:
+#     print(x)
+#     x -= 1
+# else:
+#     print('hahahaha')
+
+
+# def hodi():
+#     pass
+#
+# def zahi():
+#     pass
+#
+
+
+# from flask import Flask
+#
+# app = Flask('hodi')
+#
+#
+# @app.route('/', ['GET'])
+# def f2():
+#     return 'hello'
+#
+#
+# app.run(port='5001')
